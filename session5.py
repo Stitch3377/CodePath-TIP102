9ad611aedaa44ccab4f75fc08a98a35c17ef697d
Iv SooHoo <ivankavsh@gmail.com>
2025-07-05 18:35:57 -0400

    complete problems 4-7

--- session5.py
+++ session5.py
@@ -1,9 +1,13 @@
-# Problem 1
-# Inputs: String with (), [], {}
-# Outputs: boolean, true if valid/balance, false otherwise
-# Constraints: only (), [], {}
-# Edge cases: no brackets, empty string
+from collections import deque
+
+"""
+Session 5: Unit 3
+Breakout Problem Set - Standard Version 2
+"""
+
+
 def is_valid_post_format(posts):
+    """Problem 1"""
     stack = []
     for char in posts:
         if len(stack) == 0:
@@ -18,46 +22,29 @@ def is_valid_post_format(posts):
     return True
 
 
+print("Problem 1:")
 print(is_valid_post_format("()"))
 print(is_valid_post_format("()[]{}"))
 print(is_valid_post_format("(]"))
+print()
 
 
-def is_balanced_parentheses(s):
-    stack = []
-    for char in s:
-        if char == "(":
-            stack.append(char)
-        else:
-            if len(stack) == 0:
-                return False
-            stack.pop()
-    return len(stack) == 0
-
-
-# print(is_valid_post_format("()"))
-# print(is_valid_post_format("()[]{}"))
-# print(is_valid_post_format("(]"))
-
-
-# Problem 2
-# Inputs: Queue of comments (list of strings)
-# Outputs:
-# Constraints:
-# Edge cases:
 def reverse_comments_queue(comments):
+    """Problem 2"""
     stack = []
     for i in range(len(comments)):
         stack.append(comments.pop())
     return stack
 
 
-# print(reverse_comments_queue(["Great post!", "Love it!", "Thanks for sharing."]))
-# print(reverse_comments_queue(["First!", "Interesting read.", "Well written."]))
+print("Problem 2:")
+print(reverse_comments_queue(["Great post!", "Love it!", "Thanks for sharing."]))
+print(reverse_comments_queue(["First!", "Interesting read.", "Well written."]))
+print()
 
 
-# Problem 3
 def is_symmetrical_title(title):
+    """Problem 3"""
     stack = []
     pointer1 = 0
     pointer2 = len(title) - 1
@@ -74,5 +61,119 @@ def is_symmetrical_title(title):
     return True
 
 
-# print(is_symmetrical_title("A Santa at NASA"))
-# print(is_symmetrical_title("Social Media"))
+print("Problem 3:")
+print(is_symmetrical_title("A Santa at NASA"))
+print(is_symmetrical_title("Social Media"))
+print()
+
+
+def engagement_boost(engagements):
+    """Problem 4"""
+    length = len(engagements)
+    result = [0] * length
+    left = 0
+    right = length - 1
+    pos = length - 1
+
+    while left <= right:
+        abs_left = abs(engagements[left])
+        abs_right = abs(engagements[right])
+
+        if abs_left > abs_right:
+            result[pos] = engagements[left] ** 2
+            left += 1
+        else:
+            result[pos] = engagements[right] ** 2
+            right -= 1
+        pos -= 1
+    return result
+
+
+print("Problem 4:")
+print(engagement_boost([-4, -1, 0, 3, 10]))
+print(engagement_boost([-7, -3, 2, 3, 11]))
+print()
+
+
+def clean_post(post):
+    """Problem 5"""
+    stack = []
+
+    for letter in post:
+        if not stack:
+            stack.append(letter)
+        else:
+            prev = stack[-1]
+            if (prev.lower() == letter.lower()) and (
+                prev.isupper() and letter.islower()
+            ):
+                stack.pop()
+            elif (prev.lower() == letter.lower()) and (
+                prev.islower() and letter.isupper()
+            ):
+                stack.pop()
+            else:
+                stack.append(letter)
+
+    return "".join(stack)
+
+
+print("Problem 5:")
+print(clean_post("poOost"))
+print(clean_post("abBAcC"))
+print(clean_post("s"))
+print()
+
+
+def edit_post(post):
+    """Problem 6"""
+    result = ""
+
+    for word in post.split():
+        queue = deque()
+        for letter in word:
+            queue.appendleft(letter)
+        result += " " + "".join(queue)
+    return result[1:]
+
+
+print("Problem 6:")
+print(edit_post("Boost your engagement with these tips"))
+print(edit_post("Check out my latest vlog"))
+print()
+
+
+def post_compare(draft1, draft2):
+    """Problem 7"""
+    stack1 = []
+    stack2 = []
+
+    if len(draft1) != len(draft2):
+        return False
+
+    for char in draft1:
+        if stack1:
+            if char == "#":
+                stack1.pop()
+            else:
+                stack1.append(char)
+        else:
+            pass
+
+    for char in draft2:
+        if stack2:
+            if char == "#":
+                stack2.pop()
+            else:
+                stack2.append(char)
+        else:
+            pass
+
+    return "".join(stack1) == "".join(stack2)
+
+
+print("Problem 7:")
+print(post_compare("ab#c", "ad#c"))
+print(post_compare("ab##", "c#d#"))
+print(post_compare("a#c", "b"))
+print()
